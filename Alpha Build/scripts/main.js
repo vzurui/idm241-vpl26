c3b8fe2ec6f4b9e3eb65ed9fcc7646feaa9a9524
Vy Le <139026534+vzurui@users.noreply.github.com>
2024-10-14 21:56:11 -0400

    updates

--- Alpha Build/scripts/main.js
+++ Alpha Build/scripts/main.js
@@ -1,11 +1,32 @@
 const icon = document.getElementById("icon");
-
 const label = icon.nextElementSibling;
 
 icon.addEventListener("change", function () {
   if (icon.checked) {
-    label.textContent = "✓";
+    label.textContent = "\u2713";
   } else {
     label.textContent = "+";
+  }
+});
+
+const popover = document.getElementById("my-popover");
+
+icon.addEventListener("change", function () {
+  if (icon.checked) {
+    label.textContent = "\u2713";
+    popover.textContent = "Added to Your Library";
+    popover.showPopover();
+
+    setTimeout(() => {
+      popover.hidePopover();
+    }, 3000);
+  } else {
+    label.textContent = "+";
+    popover.textContent = "Removed from Your Library";
+    popover.showPopover();
+
+    setTimeout(() => {
+      popover.hidePopover();
+    }, 3000);
   }
 });
